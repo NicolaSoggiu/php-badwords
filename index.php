0b7a42d253962db72d25fb67233b7eab1c44aa22
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-06-06 15:36:40 +0200

    add str_replace for bad words

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+$input_text = $_GET["text"];
+$bad_word = str_replace('PHP', "***", $input_text);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,11 +10,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Badwords</title>
 </head>
 
 <body>
-
+    <h1><?= $bad_word; ?></h1>
 </body>
 
 </html>
